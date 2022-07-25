7a23ca2c939b0886408f4585b2aa40670e954100
rsura <rahul.s.sura@gmail.com>
2022-07-25 16:20:12 -0700

    Setup the main solver class

---
+++ WordHuntSolver.java
@@ -0,0 +1,11 @@
+import java.util.Set;
+import java.util.HashSet;
+
+public class WordHuntSolver{
+    private static final Set<String> englishWords = new HashSet<>(172821);    
+    private static final Set<String> foundWords = new HashSet<>(100);
+
+    private static int minWordSize = 4;
+    private static int maxWordSize = 10;
+    
+}
