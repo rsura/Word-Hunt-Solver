efd2a4a91ecf5f4779a600ff9e2ff8cc4143d4d5
rsura <rahul.s.sura@gmail.com>
2022-08-15 16:35:30 -0700

    Finished Doc Listener

--- WordHuntDocListener.java
+++ WordHuntDocListener.java
@@ -1,7 +1,10 @@
 import javax.swing.*;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import javax.swing.text.Document;
 import java.awt.*;
 
-public class WordHuntDocListener{
+public class WordHuntDocListener implements DocumentListener{
     private final JTextField field;
 	private final Component nextComponent;
 	private final Component previousComponent;
@@ -10,5 +13,36 @@ public class WordHuntDocListener{
 		this.previousComponent = previousComponent;
 		this.field = field;
 		this.nextComponent = nextComponent;
+	}
+
+    @Override
+	public void insertUpdate(DocumentEvent e) {
+		Document document = e.getDocument();
+//		nextComponent.requestFocus();
+		try {
+			String currText = document.getText(0,document.getLength());
+			if (currText.matches("[a-zA-Z]+")){
+				if (currText.length() > 1){
+					field.requestFocus();
+				} else {
+					nextComponent.requestFocus();
+				}
+				nextComponent.requestFocus();
+			} else {
+				field.requestFocus();
+			}
+		} catch (Exception ignored) {}
+	}
+
+	@Override
+	public void removeUpdate(DocumentEvent e) {
+		try {
+			field.setText(e.getDocument().getText(0,1));
+		} catch (Exception ignored) {}
+	}
+
+	@Override
+	public void changedUpdate(DocumentEvent e) {
+
 	}
 }
