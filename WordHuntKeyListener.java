64e4fe4ebca593ea44866245815ed0e3882f8a64
rsura <rahul.s.sura@gmail.com>
2022-08-15 16:39:26 -0700

    Finished key listener

--- WordHuntKeyListener.java
+++ WordHuntKeyListener.java
@@ -1,7 +1,9 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-public class WordHuntKeyListener {
+public class WordHuntKeyListener implements KeyListener {
     private final JTextField field;
 	private final Component nextComponent;
 	private final Component previousComponent;
@@ -10,5 +12,24 @@ public class WordHuntKeyListener {
 		this.previousComponent = previousComponent;
 		this.field = field;
 		this.nextComponent = nextComponent;
+	}
+
+    @Override
+	public void keyTyped(KeyEvent e) {
+
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		if (e.getKeyChar() == KeyEvent.VK_TAB || e.getKeyChar() == KeyEvent.VK_ENTER){
+			nextComponent.requestFocus();
+		} else if (e.getKeyChar() == KeyEvent.VK_TAB && e.isShiftDown()) {
+			previousComponent.requestFocus();
+		}
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+
 	}
 }
